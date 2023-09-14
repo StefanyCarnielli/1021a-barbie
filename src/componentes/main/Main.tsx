b4022da197207cadce7c83e80bad744ba3ce7741
stefanycarnielli <stefany.silva4@estudante.ifms.edu.br>
2023-09-14 06:59:44 -0400

    commit

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,17 +1,54 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
+type FilmeType={
+    id:number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
 export default function Main(){
-    let textodigitado = 'Barbie'
-    function TrataTexto(){
-        console.log("Digitado")
-        textodigitado = "BARBIE"
+   // let textodigitado = 'Barbie'
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+        //específicas
+    const [texto,setTexto]=useState("")
+
+    const filmes:FilmeType[]= [
+        {
+            id: 1,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id: 2,
+            titulo:'Filme Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/poster.png'
+        },
+        {
+            id: 3,
+            titulo:'Filme Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/dualipa.jpg'
+        }
+    ]
+
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){ 
+        console.log(e.target.value)
+        //Como eu faço para mudar o texto para "TERE"
     }
     return(
    <>
  <div className="campo_pesquisa">
    <p>Busque um filme:</p>
         <input type="text" className='botao_pesquisa' placeholder='Pesquise um Filme' onChange = {TrataTexto} />
-        <p>Digitado:</p>
+
+        
+(!texto)? <p>Resultados para: {texto} </p>
+
+        
+
     </div>
 
     
